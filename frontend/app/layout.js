0c5c0f484ebee01f01887970356faffb99574d85
Martijn Boers <martijn@plebian.nl>
2024-04-01 13:19:52 +0200

    Unify all the metadata calls

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,8 +4,24 @@ import Footer from "@/app/components/footer";
 
 export const metadata = {
     title: 'Welke Deelauto?',
-    description: 'Hulp met het kiezen van een deelauto',
-    metadataBase: new URL('https://welkedeelauto.nl')
+    metadataBase: new URL('https://welkedeelauto.nl'),
+    background_color: "#ffffff",
+    name: 'Welke Deelauto?',
+    short_name: 'Welke Deelauto?',
+    description: 'Hulp bij het kiezen van de goedkoopste deel auto',
+    start_url: '/',
+    display: "standalone",
+    theme_color: "#ffffff",
+    keywords: ["Deel autos", "Prijs", "Vergelijken", "Greenwheels", "MyWheels", "Snappcar", "Sixt Share"],
+    openGraph: {
+        title: 'Welke Deelauto',
+        description: 'Vergelijk prijzen van deel auto aanbieders',
+        images: ['https://welkedeelauto.nl/_next/image?url=%2Fopengraph-image.png&w=1200&h=675&q=75'],
+        url: 'https://welkedeelauto.nl',
+    },
+    icons: {
+        icon: '/favicon.ico',
+    }
 }
 
 export default function RootLayout({children}) {
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,20 +1,5 @@
 import Home from "@/app/components/home";
 
-export const metadata = {
-    title: "Welke Deelauto?",
-    keywords: ["Deel autos", "Prijs", "Vergelijken", "Greenwheels", "MyWheels", "Snappcar", "Sixt Share"],
-    openGraph: {
-        title: 'Welke Deelauto',
-        description: 'Vergelijk prijzen van deel auto aanbieders',
-        images: ['https://welkedeelauto.nl/_next/image?url=%2Fopengraph-image.png&w=1200&h=675&q=75'],
-        url: 'https://welkedeelauto.nl',
-    },
-    metadataBase: new URL('https://welkedeelauto.nl'),
-    icons: {
-        icon: '/favicon.ico',
-    }
-};
-
 export default function Page() {
     return <Home></Home>
 }
